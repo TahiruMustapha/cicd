ca06b546ffd8ba87af624766da87a33f5b9e2b78
Mustapha tahiru <tahirumustapha13@gmail.com>
2026-01-17 15:35:26 +0000

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/cicd",
+  assetPrefix:"/cicd/",
+  images: {
+    unoptimized:true
+  }
 };
 
 export default nextConfig;
